6e89d925fed3ccb8126cb18a2860fcf51af0d6d7
navaneethkn <navaneethkn@gmail.com>
2013-06-18 15:52:00 +0530

    First version

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-libvarnam-java
-==============
+# libvarnam-java
 
-Java bindings to libvarnam
+Provides access to `libvarnam` from a JAVA. 
+
+# Example
+
+```java
+Varnam varnam = new Varnam("/usr/local/share/varnam/vst/hi-unicode.vst");
+varnam.enableSuggestions("learnings.varnam.hi");
+List<Word> words = varnam.transliterate("hindi");
+for (Word word : words) {
+    System.out.println(word.getConfidence() + " - " + word.getText());
+}
+```
---
+++ src/varnam/Varnam.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package varnam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sun.jna.Pointer;
+import com.sun.jna.ptr.PointerByReference;
+
+/**
+ * 
+ *
+ */
+public final class Varnam {
+	
+	private final String vstfile;
+	private final Pointer handle;
+
+	public Varnam(String vstfile) throws VarnamException {
+		this.vstfile = vstfile;
+		PointerByReference varnamHandle = new PointerByReference();
+		PointerByReference errorMessage = new PointerByReference();
+		int status = VarnamLibrary.INSTANCE.varnam_init(vstfile, varnamHandle, errorMessage);
+		if (status != 0) {
+			throw new VarnamException("Error initializing varnam." + errorMessage.getValue().getString(0));
+		}
+		
+		this.handle = varnamHandle.getValue();
+		VarnamLibrary.INSTANCE.varnam_config(handle, 102, "/Users/navaneeth/projects/varnam/varnamproject.com/learnings.varnam.ml");
+	}
+	
+	public void enableSuggestions(String suggestionsFile) {
+		VarnamLibrary.INSTANCE.varnam_config(handle, 102, suggestionsFile);
+	}
+	
+	public List<Word> transliterate(String textToTransliterate) throws VarnamException {
+		PointerByReference output = new PointerByReference();
+		VarnamLibrary library = VarnamLibrary.INSTANCE;
+		int status = library.varnam_transliterate(handle, textToTransliterate, output);
+		if (status != 0) {
+			throw new VarnamException(library.varnam_get_last_error(handle));
+		}
+		
+		ArrayList<Word> words = new ArrayList<Word>();
+		Pointer result = output.getValue();
+		int totalWords = library.varray_length(result);
+		for (int i = 0; i < totalWords; i++) {
+			Pointer item = library.varray_get(result, i);
+			words.add(new Word(item));
+		}
+		
+		return words;
+	}
+	
+	public void learn(String word) throws VarnamException {
+		int status = VarnamLibrary.INSTANCE.varnam_learn(handle, word);
+		if (status != 0) {
+			throw new VarnamException(VarnamLibrary.INSTANCE.varnam_get_last_error(handle));
+		}
+	}
+
+	public String getVstfile() {
+		return vstfile;
+	}
+
+}
